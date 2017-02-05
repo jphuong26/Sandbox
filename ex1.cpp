efc0f469d0ef982033852d4d4be85d35efd48538
Julie <jphuong26@gmail.com>
2017-02-05 14:56:09 +1030

    Demonstrates the use of virtual destructors

---
+++ ex1.cpp
@@ -0,0 +1,24 @@
+// Making the parent destructor virtual will execute both destructors when 
+// delete is called on the instance of the derived class.
+
+#include <iostream>
+
+class Foo {
+public:
+	virtual ~Foo() {
+		std::cout << "Foo destructor" << std::endl;
+	}
+};
+
+class Bar : public Foo {
+public:
+	~Bar() {
+		std::cout << "Bar destructor" << std::endl;
+	}
+};
+
+int main() {
+	Foo* F = new Bar;
+	delete F;
+	return EXIT_SUCCESS;
+}
